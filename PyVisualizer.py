4f904a2422d78b212a3d1676a1dfe669b1bee701
Vincent Kraning <130092000+Vkraning@users.noreply.github.com>
2023-04-26 19:31:44 -0400

    Add files via upload
    I have been working on this project for a while I realized I should be updating when I figure out new functions. I added the settings menu today. Also this is me trying to add the features first and then I'm going to work on the looks of the GUI

---
+++ PyVisualizer.py
@@ -0,0 +1,104 @@
+from tkinter import *
+from tkinter import filedialog
+from pygame import mixer
+import os
+
+current_volume = float(0.5)
+Count = 1
+#Functions
+def play_song():
+    filename = filedialog.askopenfilename(initialdir="C:/",title="please select a file")
+    current_song = filename
+    song_title = filename.split("/")
+    song_title = song_title[-1]
+
+    try:
+        mixer.init()
+        mixer.music.load(current_song)
+        mixer.music.set_volume(current_volume)
+        mixer.music.play()
+        song_title_label.config(fg="yellow",text="Now playing:"+ str(song_title))
+        volume_label.config(fg="yellow",text="Volume:"+str(current_volume))
+    except Exception as e:
+        print(e)
+        song_title_label.config(fg="red", text="Error playing track")
+def decrease_volume():
+    try:
+        global current_volume
+        if current_volume <=0:
+            volume_label.config(fg="red", text="Volume: Muted")
+            return
+        current_volume=current_volume - float(0.1)
+        current_volume = round(current_volume,1)
+        mixer.music.set_volume(current_volume)
+        volume_label.config(fg="green", text="Volume:"+str(current_volume))
+    except Exception as e:
+        print(e)
+        song_title_label.config(fg="red", text="Track not selected")
+def increase_volume():
+    try:
+        global current_volume
+        if current_volume >=1:
+            volume_label.config(fg="green", text="Volume: Max")
+            return
+        current_volume=current_volume + float(0.1)
+        current_volume = round(current_volume,1)
+        mixer.music.set_volume(current_volume)
+        volume_label.config(fg="green", text="Volume:"+str(current_volume))
+    except Exception as e:
+        print(e)
+        song_title_label.config(fg="red", text="Track hasn't been selected")
+
+def pause():
+    try:
+        mixer.music.pause()
+    except Exception as e:
+        print(e)
+        song_title_label.config(fg="red", text ="Track hasn't been selected")
+
+def resume():
+    try:
+        mixer.music.unpause()
+    except Exception as e:
+        print(e)
+        song_title_label.config(fg="red", text ="Track hasn't been selected")
+def New_Window():
+    global Count
+    if Count < 2:
+        global newWindow
+        newWindow = Toplevel()
+        newWindow.title("New Window")
+        newWindow.geometry("200x200")
+        Label(newWindow,
+              text="This is a new window").pack()
+        Count += 1
+
+    else:
+        newWindow.destroy()
+        Count -= 1
+
+
+
+
+# Main screen
+master = Tk()
+master.title("Music Player")
+
+
+# The label
+Label(master, text="PyVizualizer", font=("Calibri", 15), fg="blue").grid(sticky='N', row=0, padx=120)
+Label(master, text="Please select one of your tracks", font=("Calibri", 15), fg="green").grid(sticky='N', row=1, )
+Label(master, text="Volume", font=("Calibri", 15), fg="green").grid(sticky='N', row=4, )
+song_title_label = Label(master, font=("Calibri", 12))
+song_title_label.grid(stick="N", row=3)
+volume_label = Label(master, font=("Calibri", 12))
+volume_label.grid(stick="N,", row=5)
+
+# buttons
+Button(master, text="Select Song", font=("Calibri", 12),command=play_song).grid(row=2, sticky="N")
+Button(master, text="Pause", font=("Calibri", 12),command=pause).grid(row=3, sticky="E")
+Button(master, text="Resume", font=("Calibri", 12),command=resume).grid(row=3, sticky="W")
+Button(master, text="-", font=("Calibri", 12), width=5,command=decrease_volume).grid(row=5, sticky="W")
+Button(master, text="+", font=("Calibri", 12), width=5,command=increase_volume).grid(row=5, sticky="E")
+Button(master, text="Settings", font=("Calibri", 12), width=10,command=New_Window).grid(row=0, sticky="E")
+master.mainloop()
