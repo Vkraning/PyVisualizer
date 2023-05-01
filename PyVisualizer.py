fbdb57b70bcc687d8b7d6bf8c7f9553a80fe7f57
Vincent Kraning <130092000+Vkraning@users.noreply.github.com>
2023-04-30 22:18:47 -0400

    I added a song box and added stop and play functions

--- PyVisualizer.py
+++ PyVisualizer.py
@@ -1,13 +1,17 @@
 from tkinter import *
 from tkinter import filedialog
 from pygame import mixer
+import pygame
 import os
+
 
 current_volume = float(0.5)
 Count = 1
+
 #Functions
 def play_song():
-    filename = filedialog.askopenfilename(initialdir="C:/",title="please select a file")
+    global filename
+    filename = filedialog.askopenfilename(initialdir="audio/",title="please select a file")
     current_song = filename
     song_title = filename.split("/")
     song_title = song_title[-1]
@@ -16,9 +20,9 @@ def play_song():
         mixer.init()
         mixer.music.load(current_song)
         mixer.music.set_volume(current_volume)
-        mixer.music.play()
-        song_title_label.config(fg="yellow",text="Now playing:"+ str(song_title))
-        volume_label.config(fg="yellow",text="Volume:"+str(current_volume))
+        Song_Box.insert(END, song_title)
+        #song_title_label.config(fg="blue",text="Now playing:"+ str(song_title))
+        volume_label.config(fg="blue",text="Volume:"+str(current_volume))
     except Exception as e:
         print(e)
         song_title_label.config(fg="red", text="Error playing track")
@@ -31,20 +35,20 @@ def decrease_volume():
         current_volume=current_volume - float(0.1)
         current_volume = round(current_volume,1)
         mixer.music.set_volume(current_volume)
-        volume_label.config(fg="green", text="Volume:"+str(current_volume))
+        volume_label.config(fg="blue", text="Volume:"+str(current_volume))
     except Exception as e:
         print(e)
-        song_title_label.config(fg="red", text="Track not selected")
+        song_title_label.config(fg="red", text="Track hasn't been selected")
 def increase_volume():
     try:
         global current_volume
         if current_volume >=1:
-            volume_label.config(fg="green", text="Volume: Max")
+            volume_label.config(fg="blue", text="Volume: Max")
             return
         current_volume=current_volume + float(0.1)
         current_volume = round(current_volume,1)
         mixer.music.set_volume(current_volume)
-        volume_label.config(fg="green", text="Volume:"+str(current_volume))
+        volume_label.config(fg="blue", text="Volume:"+str(current_volume))
     except Exception as e:
         print(e)
         song_title_label.config(fg="red", text="Track hasn't been selected")
@@ -76,7 +80,14 @@ def New_Window():
     else:
         newWindow.destroy()
         Count -= 1
-
+def play():
+    filename = Song_Box.get(ACTIVE)
+    filename = f'C:/Users/Vincent/Downloads/{filename}'
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.play(loops=0)
+def stop():
+    pygame.mixer.music.stop()
+    Song_Box.selection_clear(ACTIVE)
 
 
 
@@ -93,7 +104,10 @@ song_title_label = Label(master, font=("Calibri", 12))
 song_title_label.grid(stick="N", row=3)
 volume_label = Label(master, font=("Calibri", 12))
 volume_label.grid(stick="N,", row=5)
+volume_label.config(fg="blue", text="Volume:")
 
+Song_Box =Listbox(master, bg="black", fg='green', width=60)
+Song_Box.grid(sticky='N', row=10,)
 # buttons
 Button(master, text="Select Song", font=("Calibri", 12),command=play_song).grid(row=2, sticky="N")
 Button(master, text="Pause", font=("Calibri", 12),command=pause).grid(row=3, sticky="E")
@@ -101,4 +115,6 @@ Button(master, text="Resume", font=("Calibri", 12),command=resume).grid(row=3, s
 Button(master, text="-", font=("Calibri", 12), width=5,command=decrease_volume).grid(row=5, sticky="W")
 Button(master, text="+", font=("Calibri", 12), width=5,command=increase_volume).grid(row=5, sticky="E")
 Button(master, text="Settings", font=("Calibri", 12), width=10,command=New_Window).grid(row=0, sticky="E")
+Button(master, text="Play", font=("Calibri", 12), width=5,command=play).grid(row=8, sticky="W")
+Button(master, text="Stop", font=("Calibri", 12), width=10,command=stop).grid(row=8, sticky="E")
 master.mainloop()
