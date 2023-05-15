58dcf9803ea69fd47e006cfcf500ee1ece7ee4ef
Vincent Kraning <130092000+Vkraning@users.noreply.github.com>
2023-05-14 20:12:45 -0400

    Got rid of the animate feature
    I also changed some sizes

--- Music_Player.py
+++ Music_Player.py
@@ -5,6 +5,7 @@ import pygame
 import time
 from Visualizer import MusicVisualizer
 from Visualizer import color
+from PIL import ImageTk, Image
 
 current_volume = float(0.5)
 Count = 1
@@ -29,7 +30,7 @@ def play_song():
         volume_label.config(fg="blue", text="Volume:"+str(current_volume))
     except Exception as e:
         print(e)
-        song_title_label.config(fg="red", text="Error playing track")
+        error_label.config(fg="red", text="Error playing track")
 
 
 def decrease_volume():
@@ -44,7 +45,7 @@ def decrease_volume():
         volume_label.config(fg="blue", text="Volume:"+str(current_volume))
     except Exception as e:
         print(e)
-        song_title_label.config(fg="red", text="Track hasn't been selected")
+        error_label.config(fg="red", text="Track hasn't been selected")
 def increase_volume():
     try:
         global current_volume
@@ -57,21 +58,21 @@ def increase_volume():
         volume_label.config(fg="blue", text="Volume:"+str(current_volume))
     except Exception as e:
         print(e)
-        song_title_label.config(fg="red", text="Track hasn't been selected")
+        error_label.config(fg="red", text="Track hasn't been selected")
 
 def pause():
     try:
         mixer.music.pause()
     except Exception as e:
         print(e)
-        song_title_label.config(fg="red", text ="Track hasn't been selected")
+        error_label.config(fg="red", text ="Track hasn't been selected")
 
 def resume():
     try:
         mixer.music.unpause()
     except Exception as e:
         print(e)
-        song_title_label.config(fg="red", text ="Track hasn't been selected")
+        error_label.config(fg="red", text ="Track hasn't been selected")
 def New_Window():
     global Count
     global Song_Box
@@ -120,16 +121,22 @@ def New_Window():
         newWindow.destroy()
         Count -= 1
 def play():
-
-    filename = Song_Box.get(ACTIVE)
-    #BROKEN UNLESS My COMPUTER
-    #filename = f'C:/{filename}'
-    pygame.mixer.music.load(filename)
-    pygame.mixer.music.play(loops=0)
-
+    try:
+        filename = Song_Box.get(ACTIVE)
+        #BROKEN UNLESS My COMPUTER
+        #filename = f'C:/{filename}'
+        pygame.mixer.music.load(filename)
+        pygame.mixer.music.play(loops=0)
+    except Exception as e:
+        print(e)
+        error_label.config(fg="red", text ="Track hasn't been selected")
 def stop():
-    pygame.mixer.music.stop()
-    Song_Box.selection_clear(ACTIVE)
+    try:
+        pygame.mixer.music.stop()
+        Song_Box.selection_clear(ACTIVE)
+    except Exception as e:
+        print(e)
+        error_label.config(fg="red", text="Track hasn't been selected")
 def open_visualizer():
     # Create a MusicVisualizer window
     visualizer_window = Toplevel(master)
@@ -144,12 +151,24 @@ master = Tk()
 master.title("Music Player")
 now = time.time()
 
+# The pictures
+
+#Stop button
+stop_bnt = Image.open(f"E:\VIDEO\pause.png")
+resized = stop_bnt.resize((51, 51), Image.LANCZOS)
+new_stop = ImageTk.PhotoImage(resized)
+stop_bnt= "This is the stop button"
+#Play button
+play_bnt = Image.open(f"E:\VIDEO\play-button.png")
+resized = play_bnt.resize((51, 51), Image.LANCZOS)
+new_play = ImageTk.PhotoImage(resized)
 # The label
+
 Label(master, text="PyVizualizer", font=("Calibri", 15), fg="blue").grid(sticky='N', row=0, padx=120)
 Label(master, text="Please select one of your tracks", font=("Calibri", 15), fg="green").grid(sticky='N', row=1, )
 Label(master, text="Volume", font=("Calibri", 15), fg="green").grid(sticky='N', row=4, )
-song_title_label = Label(master, font=("Calibri", 12))
-song_title_label.grid(stick="N", row=3)
+error_label = Label(master, font=("Calibri", 12))
+error_label.grid(stick="N", row=3)
 volume_label = Label(master, font=("Calibri", 12))
 volume_label.grid(stick="N,", row=5)
 volume_label.config(fg="blue", text="Volume:")
@@ -160,34 +179,34 @@ Song_Box.grid(sticky='N', row=10,)
 
 # buttons
 Button(master, text="Select Song", font=("Calibri", 12),width=12,command=play_song).grid(row=2, sticky="N")
-Button(master, text="Pause", font=("Calibri", 12), width=10,command=pause).grid(row=3, sticky="E")
-Button(master, text="Resume", font=("Calibri", 12), width=10,command=resume).grid(row=3, sticky="W")
-Button(master, text="-", font=("Calibri", 12), width=5,command=decrease_volume).grid(row=5, sticky="W")
-Button(master, text="+", font=("Calibri", 12), width=5,command=increase_volume).grid(row=5, sticky="E")
-Button(master, text="Customize", font=("Calibri", 12), width=18,command=New_Window).grid(row=0, sticky="E")
-Button(master, text="Play", font=("Calibri", 12), width=10,command=play).grid(row=8, sticky="W")
-Button(master, text="Stop", font=("Calibri", 12), width=10,command=stop).grid(row=8, sticky="E")
+Button(master, text="pause", font=("Calibri", 12), width=6,command=pause).grid(row=3, sticky="E")
+Button(master, text="Resume", font=("Calibri", 12), width=6,command=resume).grid(row=3, sticky="W")
+Button(master, text="-", font=("Calibri", 12), width=6,command=decrease_volume).grid(row=5, sticky="W")
+Button(master, text="+", font=("Calibri", 12), width=6,command=increase_volume).grid(row=5, sticky="E")
+Button(master, text="Customize", font=("Calibri", 12), width=12,command=New_Window).grid(row=0, sticky="E")
+Button(master, image=new_play,command=play).grid(row=8, sticky="W")
+Button(master, image=new_stop,command=stop).grid(row=8, sticky="E")
 Button(master, text="Visualizer(Unfinished)", font=("Calibri", 12), width=20,command=open_visualizer).grid(row=8, sticky="N")
 
 
-#Canvas for visuals
-Canvas(master,bg="black", width=500, height=300).grid(row=20, sticky="W")
-count = 1
-size = 50
-def animate():
-    global count, size
-    #pygame.init()
-    size += 2
-    #sound = AudioSegment.from_file(filename)
-    #loudness = sound.dBFS
-    #with open(filename, "rb") as wav_file:
-        #audio_segment = AudioSegment.from_file(wav_file)
-    Label(master, text =size,font=("Calibri", 12), width=10).grid(row=21, sticky="E")
-    if count < 75:
-        size += 2
-        Canvas(master, bg="purple", width=size,height = size).grid(row=20, sticky="N")
-        count += 1
-        master.after(10, animate)
-
-Button(master, text="animate", font=("Calibri", 12), width=10, command=animate).grid(row=10, sticky="E")
+# #Canvas for visuals
+# Canvas(master,bg="black", width=500, height=300).grid(row=20, sticky="W")
+# count = 1
+# size = 50
+# def animate():
+#     global count, size
+#     #pygame.init()
+#     size += 2
+#     #sound = AudioSegment.from_file(filename)
+#     #loudness = sound.dBFS
+#     #with open(filename, "rb") as wav_file:
+#         #audio_segment = AudioSegment.from_file(wav_file)
+#     Label(master, text =size,font=("Calibri", 12), width=10).grid(row=21, sticky="E")
+#     if count < 75:
+#         size += 2
+#         Canvas(master, bg="purple", width=size,height = size).grid(row=20, sticky="N")
+#         count += 1
+#         master.after(10, animate)
+#
+# Button(master, text="animate", font=("Calibri", 12), width=10, command=animate).grid(row=10, sticky="E")
 master.mainloop()
